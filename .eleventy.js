dbd24f6d48266099f1e66ba22f1a2e891b989f8b
susanmbrake <susanmbrake@gmail.com>
2026-01-30 19:50:38 -0500

    Add Making Moves game

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/making-moves");
 
   // Create a collection of essays sorted by date (newest first)
   eleventyConfig.addCollection("essays", function(collectionApi) {
